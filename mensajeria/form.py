cfb09061f6dfbdf02685a079d03ca45e3021e59e
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-07-15 21:23:30 -0300

    Fix error auth_user

--- mensajeria/form.py
+++ mensajeria/form.py
@@ -2,6 +2,9 @@ from django import forms
 from ckeditor.fields import RichTextFormField
 from django.contrib.auth.models import User
 
+def get_usuarios():
+    return [(x,x) for x in User.objects.values_list('username', flat=True)]
+
 class CrearMensajeFormulario(forms.Form):
     texto = RichTextFormField()
-    destinatario = forms.ChoiceField(choices = [(x,x) for x in User.objects.values_list('username', flat=True)])
+    destinatario = forms.ChoiceField(choices = get_usuarios)
